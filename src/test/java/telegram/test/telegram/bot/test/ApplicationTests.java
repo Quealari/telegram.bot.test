b0f4b963e1c9e117d8c8cb2b41d1b41d551c0368
ilya <ilia.lobow@gmail.com>
2023-01-09 21:51:43 +0500

    Initial commit

---
+++ src/test/java/telegram/test/telegram/bot/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package telegram.test.telegram.bot.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
