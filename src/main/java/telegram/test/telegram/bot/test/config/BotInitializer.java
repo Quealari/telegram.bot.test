922f8ac1aa5b8fac6e14892b0aba28dd40798590
ilya <ilia.lobow@gmail.com>
2023-01-14 18:05:00 +0500

    настройка основы бота для ТГ (тесты ОК)

--- src/main/java/telegram/test/telegram/bot/test/config/BotInitializer.java
+++ src/main/java/telegram/test/telegram/bot/test/config/BotInitializer.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.DefaultEventListenerFactory;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import telegram.test.telegram.bot.test.service.TelegramBot;
 
+@Component
 public class BotInitializer {
 
     @Autowired
