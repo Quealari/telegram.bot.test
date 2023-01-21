fd70df8cd7da6370c60b92388013fa9653fffdf4
ilya <ilia.lobow@gmail.com>
2023-01-21 10:54:36 +0500

    вынес кнопки в отдельный класс Buttons.java

--- src/main/java/telegram/test/telegram/bot/test/service/TelegramBot.java
+++ src/main/java/telegram/test/telegram/bot/test/service/TelegramBot.java
@@ -1,18 +1,25 @@
 package telegram.test.telegram.bot.test.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import telegram.test.telegram.bot.test.buttons.Buttons;
 import telegram.test.telegram.bot.test.config.BotConfig;
+
 
 @Component
 @Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
 
+
     final BotConfig config;
+
+    @Autowired
+    Buttons buttons;
 
     public TelegramBot(BotConfig config) {
         this.config = config;
@@ -30,27 +37,20 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
+        long chatId = update.getMessage().getChatId();
+        sendMessage(chatId, "Какую таблетку выпить?");
+        SendMessage message = new SendMessage();
 
-            if (messageText.equals("/start")) {
-                startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-            }
-            else sendMessage(chatId, "Введите команду \"/start\"");
-        }
-    }
+        buttons.setButtons(message);
+        log.info("Replied to user" + update.getMessage().getChat().getFirstName());
 
-    public void startCommandReceived(long chatId, String name) {
-        String answer = "test";
-        sendMessage(chatId, answer);
-        log.info("Replied to user" + name);
     }
 
     public void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
+
 
         try {
             execute(message);
