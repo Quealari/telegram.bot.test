40c6da817511ed64c3ac4561dff956d639dd0bfb
ilya <ilia.lobow@gmail.com>
2023-01-21 12:16:11 +0500

    финальные правки для gradle

--- src/main/java/telegram/test/telegram/bot/test/service/TelegramBot.java
+++ src/main/java/telegram/test/telegram/bot/test/service/TelegramBot.java
@@ -3,12 +3,16 @@ package telegram.test.telegram.bot.test.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import telegram.test.telegram.bot.test.buttons.Buttons;
 import telegram.test.telegram.bot.test.config.BotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -40,17 +44,16 @@ public class TelegramBot extends TelegramLongPollingBot {
         long chatId = update.getMessage().getChatId();
         sendMessage(chatId, "Какую таблетку выпить?");
         SendMessage message = new SendMessage();
-
         buttons.setButtons(message);
-        log.info("Replied to user" + update.getMessage().getChat().getFirstName());
 
+
+        log.info("Replied to user" + update.getMessage().getChat().getFirstName());
     }
 
     public void sendMessage(long chatId, String textToSend) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText(textToSend);
-
 
         try {
             execute(message);
